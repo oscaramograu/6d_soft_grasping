30f8c25328d8ec5a6fd5ae9dd7fcfeaed68b4d03
NeuroLab <15.oscar.amo@gmail.com>
2023-05-09 16:03:44 +0200

    adapted camera thread to ros

--- impose_grasp/scripts/thread.py
+++ impose_grasp/scripts/thread.py
@@ -1,23 +1,22 @@
 import cv2
 import numpy as np
-from scipy.spatial.transform.rotation import Rotation as R
-from typing import Dict
 import os
 import time
-from threading import Thread
-from typing import List, Callable
-import open3d as o3d
+from typing import Callable
 
 from include.app.app import App
+
 from include.models.cameras.realsense_D415 import D415
-# from include.models.cameras.base_camera import Camera, CamFrame
+from include.models.cameras.base_camera import CamFrame
 from include.models.dt_object import DTObject
+from include.models.obstruction_pcd import ObstructionPcd
+
 from include.networks.pose_detection_network import PoseDetectionNetwork
 from include.lib.geometry import invert_homogeneous
 from include.lib.utils import time_stamp
 
 class CameraThread:
-    def __init__(self): #, cb_camera: Callable[[CamFrame], None] = None):
+    def __init__(self, cb_camera: Callable[[CamFrame], None] = None): #, cb_camera: Callable[[CamFrame], None] = None):
             # flags
         self.stop_flag = False
         self.settings = App().settings['Object Detection']  # relevant settings
@@ -33,14 +32,17 @@ class CameraThread:
         n_points = 512
         self.pose_detector = PoseDetectionNetwork(self.obj_type , n_points)
 
+        self.obstruction_pcd = ObstructionPcd()
+
+        self.draw_object = False
+        self.darknet_frame = None
+        self.dt = None
+        self.cb_camera = cb_camera
 
     def create_dtObject(self, obj_id):
-        n_objs = sum([x.startswith(obj_id)
-                     for x in self.dt_objectcs_list.keys()])
-        unique_obj_name = f"{obj_id}__{n_objs:02}"
         meshpath = os.path.join("data", "models", obj_id, f"{obj_id}.ply")
 
-        new_obj = DTObject(name=unique_obj_name,
+        new_obj = DTObject(name=obj_id,
                            meshpath=meshpath, show_axes=True)
         return new_obj
     
@@ -67,15 +69,33 @@ class CameraThread:
 
         self.t_last_cb_camera = time.perf_counter()
 
-    def get_dtObjDetections(self, frame):
+        self.dt = dt
+
+    def get_dtObjDetections_and_set_darknetFrame(self, frame: CamFrame):
         confidence_threshold = self.settings['confidence']
-        darknet_frame, detections, _ = self.obj_detector.image_detection(
+        self.darknet_frame, detections, _ = self.obj_detector.image_detection(
             frame.rgb.copy(), confidence_threshold, None)
         # print(detections)
         detections = [x for x in detections if x[0] == self.dt_obj_type]
         return detections
     
-    def get_bboxFromPredictedPVN(self, frame):
+    def get_bboxOfObjInFrame(self, frame: CamFrame, detected):
+        bbox = None
+        detections = self.get_dtObjDetections_and_set_darknetFrame(frame)
+        if len(detections) > 0:
+            bbox_xywh = detections[-1][2]
+            confidence = detections[-1][1]
+            bbox = self.obj_detector.yolo_bbox_2_original(
+                bbox_xywh, frame.rgb.shape[:2])
+
+        if detected:
+            # if previously detected calculate the bounding box from pvn prediction
+            # project the bounding box of the object into the image
+            bbox = self.get_bboxProjected(frame)
+
+        return bbox
+    
+    def get_bboxProjected(self, frame: CamFrame):
         affine_matrix = invert_homogeneous(
             frame.extrinsic) @ self.dt_obj.pose
         pvn_bbox = self.dt_obj.bounding_box @ affine_matrix[:3, :3].T
@@ -94,15 +114,36 @@ class CameraThread:
 
         return pvn_bbox
     
+    def detectAndGet_AffineMatrix(self,frame: CamFrame, bbox):
+        affine_matrix = None
+        if bbox is not None and self.pose_detector is not None:
+            try:
+                detected, affine_matrix = self.pose_detector.inference(bbox, frame.rgb.copy(
+                ), frame.depth, frame.intrinsic, use_icp=self.use_icp)  # affine_matrix.shape = 3x4 !
+            except Exception as e:
+                print(f"[{time_stamp()}] pose detection failed with ", e)
+        return detected, affine_matrix
+    
+    def annotate_frameWithBbox(self, frame: CamFrame, bbox, affine_matrix):
+        annotated = frame.rgb.copy()
+        if self.draw_object:
+            if bbox is not None:
+                annotated = cv2.rectangle(annotated, (int(bbox[0]), int(
+                    bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), thickness=4)
+            if self.dt_obj.active:
+                annotated = self.pose_detector.draw_obj(
+                    annotated, affine_matrix,  frame.intrinsic)
+
+        cv2.putText(annotated, f"{1/self.dt:4.1f} FPS", (10, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=3)
+
+        anno_frame = CamFrame(rgb=annotated, depth=frame.depth.copy(), 
+            intrinsic=frame.intrinsic, extrinsic=frame.extrinsic, rgb2=self.darknet_frame)
+
+        return anno_frame
+
     def main(self):
-        prev_pose = None
-        prev_omega = None
-        omega = None
-        v = None
-        prev_v = None
         frame = None
-        prior_obj_type = None
-        prior_cam = None
         detected = False
 
         while not self.stop_flag:
@@ -113,87 +154,27 @@ class CameraThread:
                 continue
 
             # Image (2D object detection/prediction)
-            bbox = None
-            detections = self.get_dtObjDetections(frame)
-            if len(detections) > 0:
-                bbox_xywh = detections[-1][2]
-                confidence = detections[-1][1]
-                bbox = self.obj_detector.yolo_bbox_2_original(
-                    bbox_xywh, frame.rgb.shape[:2])
-
-            if detected:
-                # if previously detected calculate the bounding box from pvn prediction
-                # project the bounding box of the object into the image
-                bbox = self.get_bboxFromPredictedPVN(frame)
+            bbox = self.get_bboxOfObjInFrame(self, frame, detected)
 
             # 6D pose estimation of selected object
             detected = False
-            affine_matrix = None
-            if bbox is not None and self.pose_detector is not None:
-                try:
-                    detected, affine_matrix = self.pose_detector.inference(bbox, frame.rgb.copy(
-                    ), frame.depth, frame.intrinsic, use_icp=self.use_icp)  # affine_matrix.shape = 3x4 !
-                except Exception as e:
-                    print(f"[{time_stamp()}] pose detection failed with ", e)
+            detected, affine_matrix = self.detectAndGet_AffineMatrix(frame, bbox)
 
             if detected:
                 current_pose = frame.extrinsic @ affine_matrix
-                if prev_pose is not None:
-                    v = np.linalg.norm(
-                        current_pose[:3, 3] - prev_pose[:3, 3]) / dt  # m / sec
-                    omega = R.from_matrix(
-                        current_pose[:3, :3]) * R.from_matrix(prev_pose[:3, :3].T)
-                    omega = omega.magnitude() / dt  # rad/sec
-                    #print(f"Object [v|omega]: {v:.4f}, {omega/np.pi*180:.2f}")
 
-                    if prev_v is not None:
-                        v_dot = (v - prev_v) / dt
-                        omega_dot = (omega - prev_omega) / dt
-                        #print(f"Object [v_dot|omega_dot]: {v_dot:.4f}, {omega_dot/np.pi*180:.2f}")
-
-                    vel_limit = v > 0.12
-                    omega_limit = omega > np.pi*0.8
-                    if vel_limit or omega_limit:
-                        detected = False
-                        reason = "velocity" if vel_limit else "omega"
-                        print(
-                            f"[{time_stamp()}] Using darknet because of {reason}")
-
-                prev_pose = current_pose
-                if prev_pose is not None:
-                    prev_omega = omega
-                    prev_v = v
-                else:
-                    prev_omega = None
-                    prev_v = None
-                dt_obj.show()
-                dt_obj.transform(current_pose)
+                self.dt_obj.show()
+                self.dt_obj.transform(current_pose)
 
             else:
-                dt_obj.hide()
-                prev_pose = None
-                prev_omega = None
-                prev_v = None
+                self.dt_obj.hide()
 
             # update digital twin obstruction
             self.obstruction_pcd.update_from_scene(
-                frame, dt_obj, affine_matrix)
+                frame, self.dt_obj, affine_matrix)
 
             # annotate
-            annotated = frame.rgb.copy()
-            if self.draw_object:
-                if bbox is not None:
-                    annotated = cv2.rectangle(annotated, (int(bbox[0]), int(
-                        bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 0, 0), thickness=4)
-                if dt_obj.active:
-                    annotated = poseDetection.draw_obj(
-                        annotated, affine_matrix,  frame.intrinsic)
-
-            cv2.putText(annotated, f"{1/dt:4.1f} FPS", (10, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), thickness=3)
-
-            anno_frame = CamFrame(rgb=annotated, depth=frame.depth.copy(
-            ), intrinsic=frame.intrinsic, extrinsic=frame.extrinsic, rgb2=darknet_frame)
+            anno_frame = self.annotate_frameWithBbox(frame, bbox, affine_matrix)
 
             if self.cb_camera is not None:
                 self.cb_camera(anno_frame)
